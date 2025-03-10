2b9b25534b5d8dc5f36332403572a3cfc08e1b6c
esmereina16 <emreina@escuelasproa.edu.ar>
2025-03-10 10:34:13 -0300

    Update if.py

--- if.py
+++ if.py
@@ -1,1 +1,1 @@
-
+# crear un programa que solicite al usuario igrisra un numero, si el  mismo es mayor a 10 mostrar por "pantalla es mayor a 1o" en el caso contrario mostrar por pantalla "fin del programa" 
